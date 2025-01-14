598dfa5ab76c9165d8b8012db2cfc3d5fc9c31c5
wsudo <mahdiomati@gmail.com>
2025-01-15 01:54:40 +0330

    add Most Types Of CrossEntropy  Loss function

--- littlenue/loss/cross_entropy.py
+++ littlenue/loss/cross_entropy.py
@@ -1,6 +1,6 @@
 import numpy
 
-from .loss_function import ClassificationLossFunction
+from .loss_function import ClassificationLossFunction, MultiLabelLossFunction
 
 
 class CrossEntropyLoss(ClassificationLossFunction):
@@ -176,6 +176,65 @@ class SparseCategoricalCrossEntropyLoss(ClassificationLossFunction):
         return numpy.mean(loss)
 
 
+class MultiLabelCrossEntropyLoss(ClassificationLossFunction, MultiLabelLossFunction):
+    """In multi-label classification, each input can be assigned multiple labels (as opposed to single-class assignments in multi-class classification). Binary Cross-Entropy Loss (for multi-label classification) is computed for each class independently, treating the problem as a set of binary classification tasks.
+
+    For multi-label classification, each label is independent, and the loss function is calculated as the binary cross-entropy for each label, then averaged over all labels.
+
+    Args:
+        y_true (numpy.ndarray | list): true values
+        y_pred (numpy.ndarray | list): network generated values
+        delta (float):is a threshold that defines the point where the loss function transitions from quadratic to linear.
+
+    Raises:
+        ValueError: when y_true is not type (list , numpy.ndarray)
+        ValueError: when y_pred is not type (list , numpy.ndarray)
+
+    Returns:
+        numpy.ndarray: loss between `True Values` and `Predicted Values`
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def calc(
+        self,
+        y_true: numpy.ndarray | list,
+        y_pred: numpy.ndarray | list,
+    ) -> numpy.ndarray:
+        """In multi-label classification, each input can be assigned multiple labels (as opposed to single-class assignments in multi-class classification). Binary Cross-Entropy Loss (for multi-label classification) is computed for each class independently, treating the problem as a set of binary classification tasks.
+
+        For multi-label classification, each label is independent, and the loss function is calculated as the binary cross-entropy for each label, then averaged over all labels.
+
+        Args:
+            y_true (numpy.ndarray | list): true values
+            y_pred (numpy.ndarray | list): network generated values
+            delta (float):is a threshold that defines the point where the loss function transitions from quadratic to linear.
+
+        Raises:
+            ValueError: when y_true is not type (list , numpy.ndarray)
+            ValueError: when y_pred is not type (list , numpy.ndarray)
+
+        Returns:
+            numpy.ndarray: loss between `True Values` and `Predicted Values`
+        """
+        if not isinstance(y_true, numpy.ndarray) and not isinstance(y_true, list):
+            raise ValueError(f"{type(self)}.calc 'y_true' argument must be type (list , numpy.ndarray) , {type(y_true)} passed !")
+        if not isinstance(y_pred, numpy.ndarray) and not isinstance(y_pred, list):
+            raise ValueError(f"{type(self)}.calc 'y_pred' argument must be type (list , numpy.ndarray) , {type(y_pred)} passed !")
+
+        real_y_true = y_true if isinstance(y_true, numpy.ndarray) else numpy.array(y_true)
+        real_y_pred = y_pred if isinstance(y_pred, numpy.ndarray) else numpy.array(y_pred)
+
+        # Clip predictions to avoid log(0) which leads to NaN or infinity
+        real_y_pred = numpy.clip(real_y_pred, 1e-15, 1 - 1e-15)
+
+        # Calculate Binary Cross-Entropy loss for multi-label classification
+        loss = -numpy.mean(numpy.sum(real_y_true * numpy.log(real_y_pred) + (1 - real_y_true) * numpy.log(1 - real_y_pred), axis=1))
+
+        return loss
+
+
 class BinaryCrossEntropyLoss(CrossEntropyLoss):
     """We clip the predictions to avoid taking the logarithm of 0, which is undefined. The clipping ensures predictions are in the range `[1e−15,1−1e−15]`.
     The binary cross-entropy is calculated for each sample, and the mean is returned.
@@ -324,3 +383,26 @@ def sc_cross_entropy_loss(
         numpy.ndarray: loss between `True Values` and `Predicted Values`
     """
     return SCCrossEntropyLoss() if y_true is None and y_pred is None else SCCrossEntropyLoss().calc(y_true, y_pred)
+
+
+def multilabel_cross_entropy_loss(
+    y_true: numpy.ndarray | list = None,
+    y_pred: numpy.ndarray | list = None,
+) -> MultiLabelCrossEntropyLoss | numpy.ndarray:
+    """In multi-label classification, each input can be assigned multiple labels (as opposed to single-class assignments in multi-class classification). Binary Cross-Entropy Loss (for multi-label classification) is computed for each class independently, treating the problem as a set of binary classification tasks.
+
+    For multi-label classification, each label is independent, and the loss function is calculated as the binary cross-entropy for each label, then averaged over all labels.
+
+    Args:
+        y_true (numpy.ndarray | list): true values
+        y_pred (numpy.ndarray | list): network generated values
+        delta (float):is a threshold that defines the point where the loss function transitions from quadratic to linear.
+
+    Raises:
+        ValueError: when y_true is not type (list , numpy.ndarray)
+        ValueError: when y_pred is not type (list , numpy.ndarray)
+
+    Returns:
+        numpy.ndarray: loss between `True Values` and `Predicted Values`
+    """
+    return MultiLabelCrossEntropyLoss() if y_true is None and y_pred is None else MultiLabelCrossEntropyLoss().calc(y_true, y_pred)
