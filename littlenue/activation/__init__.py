91bf4b246bff98a4731b4bd90897abb565ab19dd
wsudo <mahdiomati@gmail.com>
2025-01-14 23:36:14 +0330

    adding __init__.py to activations and put all of them in it

---
+++ littlenue/activation/__init__.py
@@ -0,0 +1,73 @@
+from .activation_function import ActivationFunction
+
+from .elu import (
+    ELU,
+    ExponentialLinearUnit,
+    elu,
+    elu_derivation,
+    exponential_linear_unit,
+    exponential_linear_unit_derivation,
+)
+
+from .hard_sigmoid import (
+    HardSigmoid,
+    hardsigmoid,
+    hardsigmoid_derivation,
+)
+
+from .hard_swish import (
+    HardSwish,
+    hardswish,
+    hardswish_derivation,
+)
+
+from .leaky_relu import (
+    LeakyRelu,
+    leaky_relu,
+    leaky_relu_derivation,
+)
+
+from .prelu import (
+    PRelu,
+    ParametricRelu,
+    prelu,
+    prelu_derivation,
+    parametric_relu,
+    parametric_relu_derivation,
+)
+
+from .relu import (
+    Relu,
+    relu,
+    relu_derivation,
+)
+
+from .sigmoid import (
+    Sigmoid,
+    sigmoid,
+    sigmoid_derivate,
+)
+
+from .softmax import (
+    Softmax,
+    softmax,
+    softmax_derivation,
+)
+
+from .softplus import (
+    Softplus,
+    softplus,
+    softplus_derivation,
+)
+
+from .swish import (
+    Swish,
+    swish,
+    swish_derivation,
+)
+
+from .tanh import (
+    Tanh,
+    tanh,
+    tanh_derivation,
+)
