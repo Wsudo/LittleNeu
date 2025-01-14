a92c92871faef3287482cd7b84d376973fa77bb2
wsudo <mahdiomati@gmail.com>
2025-01-15 00:28:29 +0330

    add MSE Loss function

---
+++ littlenue/loss/mse.py
@@ -0,0 +1,70 @@
+import numpy
+
+from .loss_function import RegressionLossFunction
+
+
+class MSELoss(RegressionLossFunction):
+    """The MSE loss is computed by first calculating the squared differences between the true values and the predicted values, and then taking the average of these squared differences.
+
+    Args:
+        y_true (numpy.ndarray | list): true values
+        y_pred (numpy.ndarray | list): network generated values
+
+    Raises:
+        ValueError: when y_true is not type (list , numpy.ndarray)
+        ValueError: when y_pred is not type (list , numpy.ndarray)
+
+    Returns:
+        numpy.ndarray: loss between `True Values` and `Predicted Values`
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def calc(
+        self,
+        y_true: numpy.ndarray | list,
+        y_pred: numpy.ndarray | list,
+    ) -> numpy.ndarray:
+        """The MSE loss is computed by first calculating the squared differences between the true values and the predicted values, and then taking the average of these squared differences.
+
+        Args:
+            y_true (numpy.ndarray | list): true values
+            y_pred (numpy.ndarray | list): network generated values
+
+        Raises:
+            ValueError: when y_true is not type (list , numpy.ndarray)
+            ValueError: when y_pred is not type (list , numpy.ndarray)
+
+        Returns:
+            numpy.ndarray: loss between `True Values` and `Predicted Values`
+        """
+        if not isinstance(y_true, numpy.ndarray) and not isinstance(y_true, list):
+            raise ValueError(f"{type(self)}.calc 'y_true' argument must be type (list , numpy.ndarray) , {type(y_true)} passed !")
+        if not isinstance(y_pred, numpy.ndarray) and not isinstance(y_pred, list):
+            raise ValueError(f"{type(self)}.calc 'y_pred' argument must be type (list , numpy.ndarray) , {type(y_pred)} passed !")
+
+        real_y_true = y_true if isinstance(y_true, numpy.ndarray) else numpy.array(y_true)
+        real_y_true = y_pred if isinstance(y_pred, numpy.ndarray) else numpy.array(y_pred)
+
+        return numpy.mean((y_true - y_pred) * 2)
+
+
+def mse_loss(
+    y_true: numpy.ndarray | list = None,
+    y_pred: numpy.ndarray | list = None,
+) -> MSELoss | numpy.ndarray:
+    """The MSE loss is computed by first calculating the squared differences between the true values and the predicted values, and then taking the average of these squared differences.
+
+    Args:
+        y_true (numpy.ndarray | list): true values
+        y_pred (numpy.ndarray | list): network generated values
+
+    Raises:
+        ValueError: when y_true is not type (list , numpy.ndarray)
+        ValueError: when y_pred is not type (list , numpy.ndarray)
+
+    Returns:
+        numpy.ndarray: loss between `True Values` and `Predicted Values`
+    """
+    return MSELoss() if y_true == None and y_pred == None else MSELoss().calc(y_true, y_pred)
